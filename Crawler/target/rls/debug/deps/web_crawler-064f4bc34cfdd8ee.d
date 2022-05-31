61372ed746022af4e0cabd99f5b42b4da44b2eaa
crustydev <ademola1145@gmail.com>
2022-05-31 15:37:15 +0100

    Add Crawler to Rust offensive security tools

---
+++ Crawler/target/rls/debug/deps/web_crawler-064f4bc34cfdd8ee.d
@@ -0,0 +1,5 @@
+/home/ademola/source/black_hat_rust/web_crawler/target/rls/debug/deps/web_crawler-064f4bc34cfdd8ee.rmeta: src/main.rs
+
+/home/ademola/source/black_hat_rust/web_crawler/target/rls/debug/deps/web_crawler-064f4bc34cfdd8ee.d: src/main.rs
+
+src/main.rs:
---
+++ Crawler/target/rls/debug/deps/web_crawler-769818cc2d585d3f.d
@@ -0,0 +1,5 @@
+/home/ademola/source/black_hat_rust/web_crawler/target/rls/debug/deps/web_crawler-769818cc2d585d3f.rmeta: src/main.rs
+
+/home/ademola/source/black_hat_rust/web_crawler/target/rls/debug/deps/web_crawler-769818cc2d585d3f.d: src/main.rs
+
+src/main.rs:
